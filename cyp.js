6d86cbd610bede199bb006b2a65cfb88d2b203b8
oscardavidgomezoyos2007 <oscardavidgomezoyos2007@gmail.com>
2026-05-24 18:42:16 -0500

    Mejoras de nuestro grupo

--- cyp.js
+++ cyp.js
@@ -31,3 +31,66 @@ function mostrarEmergente(mensaje, tipo) {
     notif.className = 'notificacion';
   }, 4000);
 }
+function calcularCostosFijos() {
+
+  let alquiler = Number(document.getElementById("alquiler").value);
+  let internet = Number(document.getElementById("internet").value);
+  let seguro = Number(document.getElementById("seguro").value);
+
+  let total = alquiler + internet + seguro;
+
+  let resultado = document.getElementById("resultadoCF");
+
+  resultado.classList.add("visible");
+
+  resultado.innerHTML =
+    "<div class='linea'>Costo Fijo Total: $" + total.toFixed(2) + "</div>";
+}
+
+function calcularCostos() {
+
+  let directos = Number(document.getElementById("directos").value);
+  let indirectos = Number(document.getElementById("indirectos").value);
+
+  let total = directos + indirectos;
+
+  let resultado = document.getElementById("resultadoCD");
+
+  resultado.classList.add("visible");
+
+  resultado.innerHTML =
+    "<div class='linea'>Costo Total: $" + total.toFixed(2) + "</div>";
+}
+
+function calcularManoObra() {
+
+  let empleados = Number(document.getElementById("empleados").value);
+  let salario = Number(document.getElementById("salario").value);
+
+  let total = empleados * salario;
+
+  let resultado = document.getElementById("resultadoMO");
+
+  resultado.classList.add("visible");
+
+  resultado.innerHTML =
+    "<div class='linea'>Costo Mano de Obra: $" + total.toFixed(2) + "</div>";
+}
+
+function calcularGanancia() {
+
+  let costo = Number(document.getElementById("costo").value);
+  let precio = Number(document.getElementById("precio").value);
+
+  let ganancia = precio - costo;
+
+  let porcentaje = (ganancia / costo) * 100;
+
+  let resultado = document.getElementById("resultadoMG");
+
+  resultado.classList.add("visible");
+
+  resultado.innerHTML =
+    "<div class='linea'>Ganancia: $" + ganancia.toFixed(2) + "</div>" +
+    "<div class='linea'>Porcentaje: " + porcentaje.toFixed(2) + "%</div>";
+}
